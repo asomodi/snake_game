964ea8b670599d4eefe4d854dfb1cc67bd086dbe
Akos Somodi <akossomodi@yahoo.com>
2020-04-04 13:17:52 +0200

    styling the page

--- board.js
+++ board.js
@@ -1,11 +1,15 @@
 // create a board
-const canvas = document.querySelector(".canvas");
-const context = canvas.getContext("2d");
+const canvas = document.querySelector('.canvas');
+const context = canvas.getContext('2d');
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
 
 // set a size the elements of board
 let scale = 30;
 let rows = canvas.height / scale;
 let columns = canvas.width / scale;
+let score = 0;
 
 var snake;
 
@@ -32,15 +36,17 @@ var snake;
 
     // always refresh the score h1
     snake.checkCollision();
-    document.querySelector(".score")
-      .innerText = snake.totalEatenApples;
-
+    document.querySelector('.score').innerText = snake.totalEatenApples;
   }, 250);
-}());
+})();
 
-// set the arrow keys, 
-window.addEventListener("keydown", ((event) => {
-  const direction = event.key.replace("Arrow", "");
+// set the arrow keys,
+window.addEventListener('keydown', (event) => {
+  const direction = event.key.replace('Arrow', '');
   // put the snake movement to them
   snake.changeDirection(direction);
-}));
+});
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
